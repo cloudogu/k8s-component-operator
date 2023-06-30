f4adfae3238700ceba63d3641a354031027a8791
Benjamin Ernst <benjamin.ernst@cloudogu.com>
2023-06-30 08:19:48 +0200

    #3 add tests for helm/client

--- pkg/helm/client_test.go
+++ pkg/helm/client_test.go
@@ -1,33 +1,48 @@
 package helm
 
-// import (
-// 	"fmt"
-// 	"github.com/stretchr/testify/require"
-// 	"helm.sh/helm/v3/pkg/registry"
-// 	"testing"
-// )
-//
-// func TestOCI(t *testing.T) {
-// 	t.Run("test mirror oci helm chart", func(t *testing.T) {
-// 		client, err := registry.NewClient()
-// 		require.NoError(t, err)
-//
-// 		harborHost := "staging-registry.cloudogu.com"
-// 		err = client.Login(harborHost, registry.LoginOptBasicAuth("todo", "todo"))
-// 		require.NoError(t, err)
-//
-// 		result, err := client.Pull(fmt.Sprintf("%s/testing/k8s-dogu-operator:0.1.0", harborHost))
-// 		require.NoError(t, err)
-// 		require.NotNil(t, result)
-// 		require.NoError(t, client.Logout(harborHost))
-//
-// 		stagexHost := "stagex.cloudogu.com/v2"
-// 		err = client.Login(stagexHost, registry.LoginOptBasicAuth("todo", "todo"))
-// 		require.NoError(t, err)
-//
-// 		push, err := client.Push(result.Chart.Data, fmt.Sprintf("%s/helmtest/k8s-dogu-operator:0.1.0", stagexHost), registry.PushOptStrictMode(false))
-// 		require.NoError(t, err)
-// 		require.NotNil(t, push)
-// 		require.NoError(t, client.Logout(stagexHost))
-// 	})
-// }
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	t.Run("should create new client", func(t *testing.T) {
+		namespace := "ecosystem"
+		debug := false
+
+		// override default controller method to retrieve a kube config
+		oldGetConfigOrDieDelegate := ctrl.GetConfigOrDie
+		defer func() { ctrl.GetConfigOrDie = oldGetConfigOrDieDelegate }()
+		ctrl.GetConfigOrDie = func() *rest.Config {
+			return &rest.Config{}
+		}
+
+		client, err := New(namespace, debug, nil)
+
+		require.NoError(t, err)
+		assert.NotNil(t, client)
+	})
+
+	t.Run("should fail to create client for ???", func(t *testing.T) {
+		// set an existing-file which is not a helm-config to ptovoke an error
+		t.Setenv("HELM_REGISTRY_CONFIG", "client.go")
+
+		namespace := "error"
+		debug := false
+
+		// override default controller method to retrieve a kube config
+		oldGetConfigOrDieDelegate := ctrl.GetConfigOrDie
+		defer func() { ctrl.GetConfigOrDie = oldGetConfigOrDieDelegate }()
+		ctrl.GetConfigOrDie = func() *rest.Config {
+			return &rest.Config{}
+		}
+
+		_, err := New(namespace, debug, nil)
+
+		require.Error(t, err)
+		assert.ErrorContains(t, err, "failed to create helm client:")
+	})
+}
