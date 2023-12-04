672203d95fa6a9cc8cfddd85f55ae14d9a1c3473
Jeremias Weber <jeremias.weber@cloudogu.com>
2023-12-04 16:04:50 +0100

    #44 remove duplicated release-args

--- release_args.sh
+++ release_args.sh
@@ -19,20 +19,3 @@ update_versions_stage_modified_files() {
 
   git add "${valuesYAML} ${componentPatchTplYAML}"
 }
-
-
-# this function will be sourced from release.sh and be called from release_functions.sh
-update_versions_modify_files() {
-  newReleaseVersion="${1}"
-  kustomizationYAML=config/manager/kustomization.yaml
-
-  yq "with(.images[] | select(.name == \"controller\") ; .newTag = \"${newReleaseVersion}\")" "${kustomizationYAML}" \
-    > tmpfile \
-    && mv tmpfile "${kustomizationYAML}"
-}
-
-update_versions_stage_modified_files() {
-  kustomizationYAML=config/manager/kustomization.yaml
-
-  git add "${kustomizationYAML}"
-}
