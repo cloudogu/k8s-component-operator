f75d614234d2d21499637a763e4a6c775d21a610
Jeremias Weber <jeremias.weber@cloudogu.com>
2023-12-04 15:52:47 +0100

    #44 update image version in values and patch-template

--- release_args.sh
+++ release_args.sh
@@ -6,6 +6,24 @@ set -o pipefail
 # this function will be sourced from release.sh and be called from release_functions.sh
 update_versions_modify_files() {
   newReleaseVersion="${1}"
+  valuesYAML=k8s/helm/values.yaml
+  componentPatchTplYAML=k8s/helm/component-patch-tpl.yaml
+
+  yq -i ".manager.image.tag = \"${newReleaseVersion}\"" "${valuesYAML}"
+  yq -i ".values.images.backup-operator |= sub(\":(([0-9]+)\.([0-9]+)\.([0-9]+)((?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))|(?:\+[0-9A-Za-z-]+))?)\", \":${newReleaseVersion}\")" "${componentPatchTplYAML}"
+}
+
+update_versions_stage_modified_files() {
+  valuesYAML=k8s/helm/values.yaml
+  componentPatchTplYAML=k8s/helm/component-patch-tpl.yaml
+
+  git add "${valuesYAML} ${componentPatchTplYAML}"
+}
+
+
+# this function will be sourced from release.sh and be called from release_functions.sh
+update_versions_modify_files() {
+  newReleaseVersion="${1}"
   kustomizationYAML=config/manager/kustomization.yaml
 
   yq "with(.images[] | select(.name == \"controller\") ; .newTag = \"${newReleaseVersion}\")" "${kustomizationYAML}" \
