5223c1e72cad3e7dfb2cf2133c4d7efad53e8dcc
Niklas Roeske <niklas.roeske@cloudogu.com>
2024-11-20 14:11:41 +0100

    #73 add test to read out environment variables

--- pkg/api/v1/ces_component_types_test.go
+++ pkg/api/v1/ces_component_types_test.go
@@ -1,9 +1,15 @@
 package v1
 
 import (
+	"bytes"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"os"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestComponent_GetHelmChartSpec(t *testing.T) {
@@ -31,6 +37,83 @@ func TestComponent_GetHelmChartSpec(t *testing.T) {
 			}
 			if got := c.GetHelmChartSpec().Namespace; !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetHelmChartSpec() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_readHelmClientTimeoutMinsEnv(t *testing.T) {
+	tests := []struct {
+		name        string
+		setEnvVar   bool
+		envVarValue string
+		want        time.Duration
+		wantLogs    bool
+		wantedLogs  string
+		logLevel    logrus.Level
+	}{
+		{
+			name:       "Environment variable not set",
+			setEnvVar:  false,
+			want:       time.Duration(15),
+			wantLogs:   true,
+			wantedLogs: "failed to read HELM_CLIENT_TIMEOUT_MINS environment variable, using default value of 15",
+			logLevel:   logrus.DebugLevel,
+		},
+		{
+			name:        "Environment variable not set correctly",
+			setEnvVar:   true,
+			envVarValue: "15//",
+			want:        time.Duration(15),
+			wantLogs:    true,
+			wantedLogs:  "failed to parse HELM_CLIENT_TIMEOUT_MINS environment variable, using default value of 15",
+			logLevel:    logrus.WarnLevel,
+		},
+		{
+			name:        "read negative environment variable",
+			setEnvVar:   true,
+			envVarValue: "-20",
+			want:        time.Duration(15),
+			wantLogs:    true,
+			wantedLogs:  "parsed value (-20) is smaller than 0, using default value of 15",
+			logLevel:    logrus.WarnLevel,
+		},
+		{
+			name:        "Successfully read environment variable",
+			setEnvVar:   true,
+			envVarValue: "20",
+			want:        time.Duration(20),
+			wantLogs:    false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.setEnvVar {
+				err := os.Setenv(helmClientTimeoutMinsEnv, tt.envVarValue)
+				require.NoError(t, err)
+			}
+			var result = time.Duration(0)
+
+			var logOutput bytes.Buffer
+
+			originalOutput := logrus.StandardLogger().Out
+			originalLevel := logrus.StandardLogger().Level
+			if tt.wantLogs {
+				logrus.StandardLogger().SetOutput(&logOutput)
+				logrus.StandardLogger().SetLevel(tt.logLevel)
+			}
+
+			result = readHelmClientTimeoutMinsEnv()
+
+			logrus.StandardLogger().SetOutput(originalOutput)
+			logrus.StandardLogger().SetLevel(originalLevel)
+
+			logs := logOutput.String()
+
+			assert.Equalf(t, tt.want, result, "readHelmClientTimeoutMinsEnv()")
+
+			if tt.wantLogs {
+				assert.Contains(t, logs, tt.wantedLogs)
 			}
 		})
 	}
--- pkg/helm/client/action_test.go
+++ pkg/helm/client/action_test.go
@@ -1,9 +1,14 @@
 package client
 
 import (
+	"bytes"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"helm.sh/helm/v3/pkg/action"
+	"os"
 	"testing"
+	"time"
 )
 
 func Test_provider_newInstall(t *testing.T) {
@@ -129,4 +134,81 @@ func Test_provider_newRollbackRelease(t *testing.T) {
 
 	// then
 	assert.NotEmpty(t, result.raw())
+}
+
+func Test_readRollbackReleaseTimeoutMinsEnv(t *testing.T) {
+	tests := []struct {
+		name        string
+		setEnvVar   bool
+		envVarValue string
+		want        time.Duration
+		wantLogs    bool
+		wantedLogs  string
+		logLevel    logrus.Level
+	}{
+		{
+			name:       "Environment variable not set",
+			setEnvVar:  false,
+			want:       time.Duration(15),
+			wantLogs:   true,
+			wantedLogs: "failed to read ROLLBACK_RELEASE_TIMEOUT_MINS environment variable, using default value of 15",
+			logLevel:   logrus.DebugLevel,
+		},
+		{
+			name:        "Environment variable not set correctly",
+			setEnvVar:   true,
+			envVarValue: "15//",
+			want:        time.Duration(15),
+			wantLogs:    true,
+			wantedLogs:  "failed to parse ROLLBACK_RELEASE_TIMEOUT_MINS environment variable, using default value of 15",
+			logLevel:    logrus.WarnLevel,
+		},
+		{
+			name:        "read negative environment variable",
+			setEnvVar:   true,
+			envVarValue: "-20",
+			want:        time.Duration(15),
+			wantLogs:    true,
+			wantedLogs:  "parsed value (-20) is smaller than 0, using default value of 15",
+			logLevel:    logrus.WarnLevel,
+		},
+		{
+			name:        "Successfully read environment variable",
+			setEnvVar:   true,
+			envVarValue: "20",
+			want:        time.Duration(20),
+			wantLogs:    false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.setEnvVar {
+				err := os.Setenv(rollbackReleaseTimeoutMinsEnv, tt.envVarValue)
+				require.NoError(t, err)
+			}
+			var result = time.Duration(0)
+
+			var logOutput bytes.Buffer
+
+			originalOutput := logrus.StandardLogger().Out
+			originalLevel := logrus.StandardLogger().Level
+			if tt.wantLogs {
+				logrus.StandardLogger().SetOutput(&logOutput)
+				logrus.StandardLogger().SetLevel(tt.logLevel)
+			}
+
+			result = readRollbackReleaseTimeoutMinsEnv()
+
+			logrus.StandardLogger().SetOutput(originalOutput)
+			logrus.StandardLogger().SetLevel(originalLevel)
+
+			logs := logOutput.String()
+
+			assert.Equalf(t, tt.want, result, "readRollbackReleaseTimeoutMinsEnv()")
+
+			if tt.wantLogs {
+				assert.Contains(t, logs, tt.wantedLogs)
+			}
+		})
+	}
 }
