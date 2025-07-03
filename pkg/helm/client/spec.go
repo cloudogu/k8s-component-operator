6177865fed9a080f63e4c3bc78123c99cb793307
Simon Klein <simon.klein@cloudogu.com>
2025-07-03 15:06:38 +0200

    #98 Remove debug code

--- pkg/helm/client/spec.go
+++ pkg/helm/client/spec.go
@@ -1,7 +1,6 @@
 package client
 
 import (
-	"fmt"
 	"github.com/cloudogu/k8s-component-operator/pkg/helm/client/values"
 	"github.com/pkg/errors"
 
@@ -13,9 +12,9 @@ import (
 // GetValuesMap returns the merged mapped out values of a chart,
 // using both ValuesYaml and ValuesOptions
 func (spec *ChartSpec) GetValuesMap(p getter.Providers) (map[string]interface{}, error) {
-	valuesYaml := map[string]interface{}{}
+	originalValuesYaml := map[string]interface{}{}
 
-	err := yaml.Unmarshal([]byte(spec.ValuesYaml), &valuesYaml)
+	err := yaml.Unmarshal([]byte(spec.ValuesYaml), &originalValuesYaml)
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to Parse ValuesYaml")
 	}
@@ -25,23 +24,13 @@ func (spec *ChartSpec) GetValuesMap(p getter.Providers) (map[string]interface{},
 		return nil, errors.Wrap(err, "Failed to Parse ValuesOptions")
 	}
 
-	result := values.MergeMaps(valuesYaml, valuesOptions)
+	mergedValuesYamlOverwrite := values.MergeMaps(originalValuesYaml, valuesOptions)
 
-	valuesYaml2 := map[string]interface{}{}
-	err = yaml.Unmarshal([]byte(spec.MappedValuesYaml), &valuesYaml2)
+	mappedValuesYaml := map[string]interface{}{}
+	err = yaml.Unmarshal([]byte(spec.MappedValuesYaml), &mappedValuesYaml)
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to Parse ValuesYaml")
 	}
 
-	fmt.Println("==================================================adasds=>>")
-
-	fmt.Printf("%v\n", valuesYaml2)
-	fmt.Println("====>")
-	fmt.Printf("%v\n", result)
-
-	finalResult := values.MergeMaps(valuesYaml2, result)
-	fmt.Println("====>")
-	fmt.Printf("%v\n", finalResult)
-
-	return finalResult, nil
+	return values.MergeMaps(mappedValuesYaml, mergedValuesYamlOverwrite), nil
 }
