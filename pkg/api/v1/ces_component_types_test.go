9fbf0183758f47c33d05ac07b10c759ab6d441f5
Marco Bergen <marco.bergen-extern@cloudogu.com>
2025-07-04 14:57:46 +0200

    [98] add unittest for good case of getMappedValuesYaml

--- pkg/api/v1/ces_component_types_test.go
+++ pkg/api/v1/ces_component_types_test.go
@@ -2,6 +2,10 @@ package v1
 
 import (
 	"context"
+	"github.com/cloudogu/k8s-component-operator/pkg/helm/client"
+	"github.com/cloudogu/k8s-component-operator/pkg/yaml"
+	"github.com/stretchr/testify/assert"
+	"helm.sh/helm/v3/pkg/chart"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"reflect"
 	"testing"
@@ -36,4 +40,141 @@ func TestComponent_GetHelmChartSpec(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_getMappedValuesYaml(t *testing.T) {
+	testCtx := context.Background()
+	t.Run("success with mapping", func(t *testing.T) {
+		component := &Component{
+			Spec: ComponentSpec{
+				MappedValues: map[string]string{
+					"mainLogLevel": "debug",
+				},
+			},
+		}
+
+		doguOpMetaData := `apiVersion: v1
+metavalues:
+  mainLogLevel:
+    name: Log-Level
+    description: The central configuration value to set the log level for this component
+    keys:
+      - path: controllerManager.env.loglevel
+        mapping:
+          debug: trace
+          info: info
+          warn: warn
+          error: error`
+
+		spec := &client.ChartSpec{}
+		helmChart := &chart.Chart{
+			Files: []*chart.File{
+				{
+					Name: mappingMetadataFileName,
+					Data: []byte(doguOpMetaData),
+				},
+			},
+		}
+
+		expectedYaml := `controllerManager:
+  env:
+    loglevel: trace
+`
+
+		mockChartGetter := NewMockChartGetter(t)
+		mockChartGetter.EXPECT().GetChart(testCtx, spec).Return(helmChart, nil)
+		mappedValuesYaml, err := getMappedValuesYaml(testCtx, component, spec, mockChartGetter, yaml.NewSerializer())
+		assert.Equal(t, expectedYaml, mappedValuesYaml)
+		assert.NoError(t, err)
+	})
+	t.Run("success without mapping", func(t *testing.T) {
+		component := &Component{
+			Spec: ComponentSpec{
+				MappedValues: map[string]string{
+					"mainLogLevel": "debug",
+				},
+			},
+		}
+
+		doguOpMetaData := `apiVersion: v1
+metavalues:
+  mainLogLevel:
+    name: Log-Level
+    description: The central configuration value to set the log level for this component
+    keys:
+      - path: controllerManager.env.loglevel`
+
+		spec := &client.ChartSpec{}
+		helmChart := &chart.Chart{
+			Files: []*chart.File{
+				{
+					Name: mappingMetadataFileName,
+					Data: []byte(doguOpMetaData),
+				},
+			},
+		}
+
+		expectedYaml := `controllerManager:
+  env:
+    loglevel: debug
+`
+
+		mockChartGetter := NewMockChartGetter(t)
+		mockChartGetter.EXPECT().GetChart(testCtx, spec).Return(helmChart, nil)
+		mappedValuesYaml, err := getMappedValuesYaml(testCtx, component, spec, mockChartGetter, yaml.NewSerializer())
+		assert.Equal(t, expectedYaml, mappedValuesYaml)
+		assert.NoError(t, err)
+	})
+	t.Run("success with multiple path", func(t *testing.T) {
+		component := &Component{
+			Spec: ComponentSpec{
+				MappedValues: map[string]string{
+					"mainLogLevel": "debug",
+				},
+			},
+		}
+
+		doguOpMetaData := `apiVersion: v1
+metavalues:
+  mainLogLevel:
+    name: Log-Level
+    description: The central configuration value to set the log level for this component
+    keys:
+      - path: controllerManager.env.loglevel
+        mapping:
+          debug: trace
+      - path: my.awesome.logLevel
+        mapping:
+          debug: all
+      - path: your.not.so.awesome.path`
+
+		spec := &client.ChartSpec{}
+		helmChart := &chart.Chart{
+			Files: []*chart.File{
+				{
+					Name: mappingMetadataFileName,
+					Data: []byte(doguOpMetaData),
+				},
+			},
+		}
+
+		expectedYaml := `controllerManager:
+  env:
+    loglevel: trace
+my:
+  awesome:
+    logLevel: all
+your:
+  not:
+    so:
+      awesome:
+        path: debug
+`
+
+		mockChartGetter := NewMockChartGetter(t)
+		mockChartGetter.EXPECT().GetChart(testCtx, spec).Return(helmChart, nil)
+		mappedValuesYaml, err := getMappedValuesYaml(testCtx, component, spec, mockChartGetter, yaml.NewSerializer())
+		assert.Equal(t, expectedYaml, mappedValuesYaml)
+		assert.NoError(t, err)
+	})
 }
