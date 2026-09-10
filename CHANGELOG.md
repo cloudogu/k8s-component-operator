3b70396369e422f8a76b72980d1f8f85e58577dd
Henry Bobka <henry.bobka@cloudogu.com>
2026-09-10 18:25:16 +0200

    #124 Fix SonarQube project version updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+- Update the SonarQube project version during releases to reset the new-code baseline
+
 ## [v1.15.0] - 2026-09-10
 ### Changed
 - [#121] Implementation of an LRU cache for Helm charts to prevent peak loads in the Harbor registry
--- release_args.sh
+++ release_args.sh
@@ -8,14 +8,17 @@ update_versions_modify_files() {
   newReleaseVersion="${1}"
   valuesYAML=k8s/helm/values.yaml
   componentPatchTplYAML=k8s/helm/component-patch-tpl.yaml
+  sonarProjectProperties=sonar-project.properties
 
   ./.bin/yq -i ".manager.image.tag = \"${newReleaseVersion}\"" "${valuesYAML}"
   ./.bin/yq -i ".values.images.componentOperator |= sub(\":(([0-9]+)\.([0-9]+)\.([0-9]+)((?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))|(?:\+[0-9A-Za-z-]+))?)\", \":${newReleaseVersion}\")" "${componentPatchTplYAML}"
+  sed -i "s/^sonar.projectVersion=.*/sonar.projectVersion=${newReleaseVersion}/" "${sonarProjectProperties}"
 }
 
 update_versions_stage_modified_files() {
   valuesYAML=k8s/helm/values.yaml
   componentPatchTplYAML=k8s/helm/component-patch-tpl.yaml
+  sonarProjectProperties=sonar-project.properties
 
-  git add "${valuesYAML}" "${componentPatchTplYAML}"
+  git add "${valuesYAML}" "${componentPatchTplYAML}" "${sonarProjectProperties}"
 }
